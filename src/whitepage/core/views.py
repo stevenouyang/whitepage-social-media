7cbf3b2bcf942d0f5ad2b6f1ced3d5fb5211725a
stevenouyang <brostevee@gmail.com>
2023-12-26 16:47:01 +0700

    feat: auth

--- src/whitepage/core/views.py
+++ src/whitepage/core/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required(login_url='signin')
 def index(request):
   return render(request, 'index.html')
 
@@ -45,4 +47,28 @@ def signup(request):
     return render(request, 'signup.html')
   
 def signin(request):
-  return render(request, 'signin.html')
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    print(username, password)    
+    user = auth.authenticate(username=username, password=password)
+    
+    if user is not None:
+      auth.login(request, user)
+      return redirect('/')
+    
+    else:
+      messages.info(request, 'Credentials are invalid')
+      return redirect('signin')
+    
+
+  else:  
+    return render(request, 'signin.html')
+  
+def logout(request):
+  auth.logout(request)
+  return redirect('signin')
+
+@login_required(login_url='signin')
+def settings(request):
+  return render(request, 'setting.html')
